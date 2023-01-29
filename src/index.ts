06bdd440518590f6b03d0798d5938a69d45e0736
Sergi Casquero <sergicasquero@gmail.com>
2023-01-29 01:02:32 +0100

    add function to render card list

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import MainComponent from "./components/MainComponent/MainComponent.js";
 import HeaderComponent from "./components/HeaderComponent/HeaderComponent.js";
-import getPokemonById from "./ts/getPokemonById.js";
 import CardComponent from "./components/CardComponent/CardComponent.js";
-import { type PokeInfoStructure } from "./ts/types.js";
+import type { PokemonListStructure, PokeInfoStructure } from "./ts/types.js";
 import CardListComponent from "./components/CardListComponent/CardListComponent.js";
+import getPokemonById from "./ts/getPokemonById.js";
+import getPokemonList from "./ts/getPokemonList.js";
+
+const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 
 const rootContainer: HTMLElement = document.querySelector(".root")!;
 
@@ -17,9 +20,12 @@ const cardList = new CardListComponent(main.domElement);
 cardList.render();
 
 const generateCard = async () => {
-  const pokemon: PokeInfoStructure = await getPokemonById(5);
-  const card = new CardComponent(cardList.domElement, pokemon);
-  card.render();
+  const pokemonList: PokemonListStructure = await getPokemonList(apiUrl);
+  pokemonList.results.forEach(async (pokemon, id) => {
+    const pokemonInfo: PokeInfoStructure = await getPokemonById(apiUrl, id);
+    const card = new CardComponent(cardList.domElement, pokemonInfo);
+    card.render();
+  });
 };
 
 await generateCard();
